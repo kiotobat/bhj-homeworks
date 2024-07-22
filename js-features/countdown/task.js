6169a7be3c9ea15a0f19239853cb911a9a76b223
Spartak I. <lambar926@gmail.com>
2024-07-22 17:35:23 +0300

    second commit countdown

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -2,6 +2,7 @@ const timeValue = function () {
     let timer = document.getElementById("timer");
     if (timer.textContent === "0") {
         alert("Вы победили в конкурсе");
+        clearInterval(timerInterval);
     } else {
         timer.textContent--;
     }
