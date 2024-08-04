0a6ed1d845ca2357a5540c2434bb079d660a42bc
Spartak I. <lambar926@gmail.com>
2024-08-04 20:50:32 +0300

    first commit keysolo

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,22 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    const symbolComparison = (event) => {
+      let key = `${event.key}`;
+
+      if (key === this.currentSymbol.textContent) {
+        this.success();
+        return;
+      }
+
+      if (key === 'Shift' || 'Alt' || 'Control' || 'CapsLock') {
+        return;
+      }
+
+      this.fail();
+    };
+
+    document.addEventListener("keyup", symbolComparison);
   }
 
   success() {
